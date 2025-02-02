cff05f1f8cbde7eefea77523ae2528345004907c
Success Enehikhuere <enehikhueresuccess@gmail.com>
2025-02-02 22:49:32 +0100

    "update navbar"

--- components/Navigation/Nav.tsx
+++ components/Navigation/Nav.tsx
@@ -3,9 +3,10 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { BiCycling, BiShoppingBag } from 'react-icons/bi'
+import { BiCycling } from 'react-icons/bi'
 import { HiBars3BottomRight } from 'react-icons/hi2'
 import { TiShoppingCart } from 'react-icons/ti'
+import logo from '@/public/images/logo.png'
 
 interface props {
   openNav: () => void
@@ -13,32 +14,35 @@ interface props {
 
 const Nav = ({openNav}:props) => {
   return (
-    <div className="h-[12vh] bg-white">
+    <div className="bg-white">
       <div className="sm:w-[90%] w-[95%] mx-auto flex h-[100%] items-center justify-between">
         {/* logo */}
         <div>
-          <Image src="/images/logo.png" alt="logo" width={170} height={70} />
+          <Image src={logo} alt="logo" width={160} height={50} />
         </div>
 
         {/* nav links */}
         <ul className="hidden lg:flex items-center space-x-8">
-          <li className="text-xl font-[600] hover:text-red-600">
+          <li className="text-xl font-medium hover:text-red-600">
             <Link href="/">Home</Link>
           </li>
-          <li className="text-xl font-[600] hover:text-red-600">
+          <li className="text-xl font-medium hover:text-red-600">
             <Link href="/cakes">Cakes</Link>
           </li>
-          <li className="text-xl font-[600] hover:text-red-600">
-            <Link href="/">Valentine - Feb 14th</Link>
+          <li className="text-xl font-medium hover:text-red-600">
+            <Link href="/">Valentine</Link>
           </li>
-          <li className="text-xl font-[600] hover:text-red-600">
+          <li className="text-xl font-medium hover:text-red-600">
             <Link href="/">Combos</Link>
           </li>
-          <li className="text-xl font-[600] hover:text-red-600">
+          <li className="text-xl font-medium hover:text-red-600">
             <Link href="/">Occasion</Link>
           </li>
-          <li className="text-xl font-[600] hover:text-red-600">
+          <li className="text-xl font-medium hover:text-red-600">
             <Link href="/">Contact</Link>
+          </li>
+          <li className="text-xl font-medium hover:text-red-600">
+            <Link href="/">Login</Link>
           </li>
         </ul>
 
@@ -55,7 +59,10 @@ const Nav = ({openNav}:props) => {
               <TiShoppingCart className="w-[1.3rem] h-[1.3rem] sm:w-[1.7rem] sm:h-[1.7rem]" />
             </span>
           </button>
-          <HiBars3BottomRight className="lg:hidden w-[2rem] h-[2rem] text-black" onClick={openNav}/>
+          <HiBars3BottomRight
+            className="lg:hidden w-[2rem] h-[2rem] text-black"
+            onClick={openNav}
+          />
         </div>
       </div>
     </div>
