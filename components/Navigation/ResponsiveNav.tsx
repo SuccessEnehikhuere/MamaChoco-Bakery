4473e107cb57347d6e43bf48dce3a7083f8cf0b9
Success Enehikhuere <enehikhueresuccess@gmail.com>
2025-02-02 22:50:14 +0100

    "update navbar"

--- components/Navigation/ResponsiveNav.tsx
+++ components/Navigation/ResponsiveNav.tsx
@@ -3,17 +3,15 @@
 import React, { useState } from 'react'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
-import NavHeader from '../NavHeader'
 
 const ResponsiveNav = () => {
-  const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(false)
 
   const openNavHandler = () => setShowNav(true)
-   const closeNavHandler = () => setShowNav(false)
+  const closeNavHandler = () => setShowNav(false)
 
   return (
     <div>
-      <NavHeader />
       <Nav openNav={openNavHandler} />
       <MobileNav showNav={showNav} closeNav={closeNavHandler} />
     </div>
